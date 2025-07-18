2fed3de7d6179c49b551223c8a8c4f91bef7ee1d
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2025-07-18 11:24:59 -0500

    banner added

--- client/src/components/CarCard.jsx
+++ client/src/components/CarCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { assets } from '../assets/assets'
-
+import { useNavigate } from 'react-router-dom'
 
 const CarCard = ({car}) => {
   const currency =import.meta.env.VITE_CURRENCY
+  const navigate = useNavigate();
   return (
-    <div className='group rounded-xl overflow-hidden
+    <div onClick={()=>{navigate(`/car-details/${car._id}`);scroll(0,0)}} className='group rounded-xl overflow-hidden
     shadow-lg hover:translate-y-1 transition-all
     duration-500 cursor-pointer'>
     
@@ -14,7 +15,7 @@ const CarCard = ({car}) => {
       className='w-full h-full object-cover
        transition-transform duration-500 
        group-hover:scale-105'/>
-       {car.isAvailable && <p className='absolute top-4 left-4 
+       {car.isAvaliable && <p className='absolute top-4 left-4 
        bg-primary/90  text-white text-xs px-2.5 py-1 rounded-full'>Available now</p>}
        <div className='absolute bottom-4 right-4 bg-black/80 backdrop-blur-sm
        text-white px-3 py-2 rounded-lg'>
@@ -29,7 +30,9 @@ const CarCard = ({car}) => {
           <h3 className='text-lg font-medium'>{car.brand} {car.model}</h3>
           <p className='text-muted-foreground text-sm '>{car.category} .{car.year}</p>
         </div>
+        
       </div>
+
       <div className='mt-4 grid grid-cols-2 gap-y-2 text-gray-600'>
           <div className='flex items-center text-sm text-muted-foreground'>
             <img src={assets.users_icon} alt="" className='h-4 mr-2'/>
