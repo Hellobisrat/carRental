116aa3bfdc8719f265e0a2d1ca62150a1edefca4
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2025-07-28 15:22:06 -0500

    sidebar ui completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,11 @@ import CarDetails from './pages/CarDetails';
 import Home from './pages/Home.jsx'
 import Footer from './components/Footer.jsx';
 import MyBookings from './pages/MyBookings.jsx';
+import Layout from './pages/owner/Layout.jsx';
+import Dashboard from './pages/owner/Dashboard.jsx';
+import AddCar from './pages/owner/AddCar.jsx';
+import ManageCar from './pages/owner/ManageCar.jsx';
+import ManageBooking from './pages/owner/ManageBooking.jsx';
 
 const App = () => {
   const [setShowLogin]=useState(false);
@@ -19,6 +24,16 @@ const App = () => {
         <Route path='/car-details/:id' element ={<CarDetails/>}/>
         <Route path='/cars' element ={<Cars/>}/>
         <Route path='//my-bookings' element={<MyBookings/>}/>
+        <Route path='/owner' element={<Layout/>}>
+        <Route index element={<Dashboard/>}/>
+        <Route path='add-car' element={<AddCar/>}/>
+        <Route path='manage-cars' element={<ManageCar/>}/>
+        <Route path='manage-booking' element={<ManageBooking/>}/>
+
+        
+  
+        
+        </Route>
       </Routes>
       {!isOwnerPath && <Footer/>}
     </>
