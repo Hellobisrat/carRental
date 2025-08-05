fba4fc7f8c75866d3b75e227573b0715a976c604
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2025-08-05 16:43:09 -0500

    carController added

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js";
-
+import Car from "../models/Car.js";
 
 export const changeRoleToOwner = async (req,res)=>{
   try {
@@ -18,9 +18,71 @@ export default addCar = async(req,res)=>{
     const {_id}=req.user;
     let car = JSON.parse(req.body.carData)
     const imageFile = req.file;
+    await Car.create({...car,owner:_id,image})
     
   } catch (error) {
     console.log(error);
     res.json({sucess:false,message:error.message})
+  }
+}
+
+export const getOwnerCars = async (req,res)=>{
+  try {
+    const {_id}=req.user;
+    const cars = await Car.find({owner:_id})
+    res.json({sucess:true,cars})
+  } catch (error) {
+    res.json({success:false,message:error.message});
+    
+  }
+}
+
+export const toggleCarAvailability = async(req,res)=>{
+  try {
+    const {_id}=req.user;
+    const {carId} =req.body
+    const car = await Car.findById(carId)
+    if(car.owner.toString()!==_id.toString()){
+      return res.json({success: false, message:"Unauthorized"})
+    }
+    car.isAvaliable =!car.isAvaliable;
+    await car.save();
+    res.json({sucess:true,message:'Availablity toggled'})
+  } catch (error) {
+    res.json({success:false,message:error.message});
+    
+  }
+}
+
+export const deleteCar = async(req,res)=>{
+  try {
+    const {_id}=req.user;
+    const {carId} =req.body
+    const car = await Car.findById(carId)
+    if(car.owner.toString()!==_id.toString()){
+      return res.json({success: false, message:"Unauthorized"})
+    }
+    car.owner =null;
+    car.isAvaliable = false
+    await car.save();
+    res.json({sucess:true,message:'Car Removed'})
+  } catch (error) {
+    res.json({success:false,message:error.message});
+    
+  }
+}
+
+export const getDashboardData = async (req,res)=>{
+  try {
+    const {_id,role}=req.user;
+    if(role !== "owner"){
+      return res.json({success:false,message:"unauthorized"})
+    }
+    const cars = await Car.find({owner:_id})
+    
+  } catch (error) {
+     res.json({success:false,message:error.message});
+    
+    
   }
 }
--- server/routes/ownerRoutes.js
+++ server/routes/ownerRoutes.js
@@ -1,8 +1,13 @@
 import express from 'express';
-import { changeRoleToOwner } from '../controllers/ownerController.js';
+import { addCar,changeRoleToOwner, deleteCar, getOwnerCars, toggleCarAvailability } from '../controllers/ownerController.js';
+import upload from '../middleware/multer.js';
+import { protect } from '../middleware/auth.js';
 
 const ownerRouter = express.Router();
 
-ownerRouter.post("/change-role",protect,changeRoleToOwner)
-
+ownerRouter.post("/change-role",protect,changeRoleToOwner);
+ownerRouter.post('/add-car',upload.single(Image),protect,addcar);
+ownerRouter.get('/cars',protect,getOwnerCars)
+ownerRouter.post('/toggle-car',protect,toggleCarAvailability);
+ownerRouter.post('/delete-car',protect,deleteCar)
 export default ownerRouter
